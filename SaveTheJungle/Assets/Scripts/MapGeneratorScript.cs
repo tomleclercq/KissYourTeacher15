745ba3a8ce0191e4c7cc99377b885f00206a6a66
Tom Leclercq <leclercqtom@live.be>
2015-10-10 15:34:34 +0200

    Dev

--- SaveTheJungle/Assets/Scripts/MapGeneratorScript.cs
+++ SaveTheJungle/Assets/Scripts/MapGeneratorScript.cs
@@ -7,6 +7,15 @@ public class MapGeneratorScript : MonoBehaviour {
     public float countY;
 
     public GameObject grass;
+    public GameObject Bottom;
+    public GameObject BottomRight;
+    public GameObject BottomLeft;
+    public GameObject Top;
+    public GameObject TopRight;
+    public GameObject TopLeft;
+    public GameObject Left;
+    public GameObject Right;
+
     GameObject tile;
     Vector2 position;
     Vector2 grassTileSize;
@@ -38,7 +47,33 @@ public class MapGeneratorScript : MonoBehaviour {
             {
                 for (int y = 0; y < countY; y++)
                 {
-                    tile = GameObject.Instantiate(grass) as GameObject;
+                    if (y == 0)
+                    {
+                        if (x == 0)
+                            tile = GameObject.Instantiate(BottomLeft) as GameObject;
+                        else
+                            if (x == countX - 1)
+                                tile = GameObject.Instantiate(BottomRight) as GameObject;
+                            else
+                            tile = GameObject.Instantiate(Bottom) as GameObject;
+                    }
+                    else if (y == countY - 1)
+                            if (x == 0)
+                                tile = GameObject.Instantiate(TopLeft) as GameObject;
+                            else
+                                if (x == countX - 1)
+                                    tile = GameObject.Instantiate(TopRight) as GameObject;
+                                else
+                                    tile = GameObject.Instantiate(Top) as GameObject;
+                            else
+                        if (x == 0)
+                            tile = GameObject.Instantiate(Left) as GameObject;
+                        else
+                            if (x == countX - 1)
+                                tile = GameObject.Instantiate(Right) as GameObject;
+                            else
+                            tile = GameObject.Instantiate(grass) as GameObject;
+
                     position.x = x * grassTileSize.x - ((countX / 2) * grassTileSize.x - grassTileSize.x / 2);
                     position.y = y * grassTileSize.y - ((countY / 2) * grassTileSize.y - grassTileSize.y / 2);
                     tile.transform.position = position;
