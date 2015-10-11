f0dad079c3dc0a7ea6f211ab081f8381fa8decf2
yasetov <yasetov@gmail.com>
2015-10-11 12:46:51 +0200

    Trapped animals get free

--- SaveTheJungle/Assets/Scripts/Traps/Trap.cs
+++ SaveTheJungle/Assets/Scripts/Traps/Trap.cs
@@ -5,7 +5,7 @@ public class Trap : MonoBehaviour {
 
 	public ToolIndex requiredTool;
 	public GameObject cluePrefab = null;
-//	public GameObject animalPrefab;
+	public GameObject animal;
 	public GameObject book;
 
 	private GameObject clueInstance = null;
@@ -17,20 +17,26 @@ public class Trap : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+//		if (animal) {
+//			if (animal.activeSelf) {
+//				animal.GetComponent<Rigidbody2D> ().MovePosition(Vector3.down * Time.deltaTime);
+//			}
+//		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if((other.tag == "Player") && (cluePrefab != null)){
-			if(!(other.GetComponent<Player>().HasTool(requiredTool))){
+		if(other.tag == "Player"){
+			if((requiredTool == ToolIndex.hand) || (other.GetComponent<Player>().HasTool(requiredTool))){
+				GetComponent<SpriteRenderer>().enabled = false;
+				//				book.SetActive(true);
+				//				Time.timeScale = 0;
+				foreach(Transform child in transform){
+					child.gameObject.SetActive(true);
+				}
+//				StartCoroutine(AnimalLeave (GetComponentInChildren<Rigidbody2D>()));
+			}else if (cluePrefab != null){
 				clueInstance = Instantiate(cluePrefab, transform.position, Quaternion.identity) as GameObject;
 				clueInstance.transform.position += Vector3.down * 2;
-			}else{
-//				GetComponent<Animator>().SetTrigger("OpenCage");
-				GetComponent<SpriteRenderer>().enabled = false;
-				book.SetActive(true);
-				transform.GetChild(0).gameObject.SetActive(true);
-//				Time.timeScale = 0;
 			}
 		}
 	}
@@ -42,6 +48,13 @@ public class Trap : MonoBehaviour {
 			}else{
 				//GetComponent<Animator>().SetTrigger("StopAnim");
 			}
+		}
+	}
+
+	IEnumerator AnimalLeave(Rigidbody2D animalRB){
+		while (animalRB.gameObject) {
+			animalRB.MovePosition(Vector3.down);
+			yield return new WaitForSeconds(1);
 		}
 	}
 }
