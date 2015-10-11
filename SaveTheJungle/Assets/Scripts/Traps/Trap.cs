73431126f6f4fdde2905cbd52a6c88d14551e2e5
DavEat <david.mestdagh@hotmail.com>
2015-10-11 13:52:00 +0200

    Ending

--- SaveTheJungle/Assets/Scripts/Traps/Trap.cs
+++ SaveTheJungle/Assets/Scripts/Traps/Trap.cs
@@ -34,7 +34,7 @@ public class Trap : MonoBehaviour {
 					child.gameObject.SetActive(true);
 				}
 //				StartCoroutine(AnimalLeave (GetComponentInChildren<Rigidbody2D>()));
-			}else if (cluePrefab != null){
+			}else if (cluePrefab != null && clueInstance == null){
 				clueInstance = Instantiate(cluePrefab, transform.position, Quaternion.identity) as GameObject;
 				clueInstance.transform.position += Vector3.down * 2;
 			}
@@ -45,6 +45,8 @@ public class Trap : MonoBehaviour {
 		if (other.tag == "Player"){
 		    if (clueInstance != null){
 				GameObject.Destroy(clueInstance);
+				clueInstance.transform.localScale = new Vector2 (0,0);
+				print (clueInstance.name);
 			}else{
 				//GetComponent<Animator>().SetTrigger("StopAnim");
 			}
